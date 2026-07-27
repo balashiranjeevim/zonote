60e23c09dfc94314ecf1d1972634908119b52dd5
balashiranjeevim <balashiranjeevim@gmail.com>
2026-07-27 22:14:26 +0530

    Updated some files, setuped shadecn and trunk extention

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+ 
 export default function Home() {
   return (
-    <p className="text-red-500 font-bold">Hello Zonote</p>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
